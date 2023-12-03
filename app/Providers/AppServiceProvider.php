6e860f95ff06e0e8879464428c4689f30741fb79
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2023-12-03 20:43:51 +0100

    change service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,17 +7,26 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @param UrlGenerator $url
+     * @return void
+     */
     public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
-    }
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
     }
 }
