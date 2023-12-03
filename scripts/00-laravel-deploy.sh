aba989e9735e973f8cbed0b681d7c28d361626f9
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2023-12-03 21:04:25 +0100

    change buil script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -10,7 +10,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
-
-echo "Running seeding..."
-php artisan db:seed
+php artisan migrate:fresh --seed
