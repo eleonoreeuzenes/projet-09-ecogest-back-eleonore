fd7d8b7529818f4b5e380c917769e701c6251f13
Eleonore Euzenes <eleonore.euzenes@gmail.com>
2023-12-03 18:50:13 +0100

    change to fit render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,17 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+    }
     /**
      * Register any application services.
      */
